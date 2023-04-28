7b72e29626a3e89ade1d98393e12a1ec2cef0c8a
DanielleSmith0 <Danielahsmith@icloud.com>
2023-04-28 11:08:45 -0600

    Initial Commit

---
+++ Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  DMTableViewIntro
+//
+//  Created by Danielle Smith on 4/27/23.
+//
+
+struct K {
+    static let cellIdentifier = "ReusableCell"
+}
---
+++ Models/Expenses.swift
@@ -0,0 +1,10 @@
+//
+//  Expenses.swift
+//  DMTableViewIntro
+//
+//  Created by Danielle Smith on 4/27/23.
+//
+
+import Foundation
+
+
